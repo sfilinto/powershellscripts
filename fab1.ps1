a7d3d9bd48dd45c13b8455d281e26594d386b4f4
Sachin Filinto <sfilinto@yahoo.com>
2020-02-29 00:18:07 +0800

    fabrikam

---
+++ fab1.ps1
@@ -0,0 +1,9 @@
+﻿# Create DC
+$dfqdn = "fabrikam.com"
+$dnetb = "FABRIKAM"
+$fmode = "Win2012"
+$dmode = "Win2012"
+$password = ConvertTo-SecureString "Password123#" -AsPlainText -Force
+
+Install-windowsfeature -name AD-Domain-Services –IncludeManagementTools
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode $dmode -DomainName $dfqdn -DomainNetbiosName $dnetb -ForestMode $fmode -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true -safemodeadministratorpassword $password
