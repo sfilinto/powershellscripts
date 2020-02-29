07dc320c8021605c2d1b4ba9b509cbe13b614a9d
Sachin Filinto <sfilinto@yahoo.com>
2020-02-29 13:36:00 +0800

    Update con2.ps1

--- con2.ps1
+++ con2.ps1
@@ -1,7 +1,7 @@
 ﻿#Domain Join
 Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses ("192.168.1.4","8.8.8.8")
 $dfqdn = "contoso.com"
-$username = "CONTOSO\fabadmin" 
+$username = "CONTOSO\conadmin" 
 $password = ConvertTo-SecureString "Password123#" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
 
