990f773ce4e7a9a39e016ba65a5bd17e1421edc1
Sachin Filinto <sfilinto@yahoo.com>
2020-06-26 23:00:20 +0800

    sfg

--- con1.ps1
+++
@@ -1,9 +0,0 @@
-﻿# Create DC
-$dfqdn = "contoso.com"
-$dnetb = "CONTOSO"
-$fmode = "Win2012"
-$dmode = "Win2012"
-$password = ConvertTo-SecureString "Password123#" -AsPlainText -Force
-
-Install-windowsfeature -name AD-Domain-Services –IncludeManagementTools
-Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode $dmode -DomainName $dfqdn -DomainNetbiosName $dnetb -ForestMode $fmode -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true -safemodeadministratorpassword $password
--- con2.ps1
+++
@@ -1,8 +0,0 @@
-﻿#Domain Join
-Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses ("192.168.1.4","8.8.8.8")
-$dfqdn = "contoso.com"
-$username = "CONTOSO\conadmin" 
-$password = ConvertTo-SecureString "Password123#" -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($username,$password)
-
-Add-Computer -DomainName $dfqdn -Credential $credential -Restart -Force
